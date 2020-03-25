61cbc73217c07c1355acdb8eee3d66bc4aa0810e
ruifgdias <ruifgdias@sapo.pt>
2020-03-25 18:45:47 +0000

    format Logon Page

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,25 +1,23 @@
-import  React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom'
 import api from '../../services/api';
-
 import './styles.css'
-
 import logoImg from '../../assets/logo.svg'
 import heroesImg from '../../assets/heroes.png'
 import { FiLogIn } from 'react-icons/fi'
 
-export default function Logon(){
+export default function Logon() {
     const [id, setId] = useState('');
     const history = useHistory();
 
     async function handleLogin(e) {
         e.preventDefault();
-        
+
         try {
             const resp = await api.post('session', { id })
 
-            localStorage.setItem('ongId',id);
-            localStorage.setItem('ongName',resp.data.ong.name);
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', resp.data.ong.name);
 
             //Navigation
             history.push('/profile');
@@ -29,32 +27,32 @@ export default function Logon(){
     }
 
     return (
-       <div className="logon-containner">
-           <section className="form">
-               <img src={logoImg} alt="logo" />
+        <div className="logon-containner">
+            <section className="form">
+                <img src={logoImg} alt="logo" />
 
 
-               <form onSubmit={handleLogin}>
-                   <h1>Login</h1>
-                   <input 
-                        placeholder="Your Id" 
+                <form onSubmit={handleLogin}>
+                    <h1>Login</h1>
+                    <input
+                        placeholder="Your Id"
                         type="text"
                         value={id}
                         onChange={e => setId(e.target.value)}
                     />
-                   <button 
+                    <button
                         className="button"
                         type="submit">
                         Enter
                     </button>
 
-                   <Link className="backlink" to="/register">
-                       <FiLogIn size={16} color="#E02041" />
+                    <Link className="backlink" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
                        I want to Join!
                    </Link>
-               </form>
-           </section>
-           <img src={heroesImg} alt="heroes" />
-       </div>
+                </form>
+            </section>
+            <img src={heroesImg} alt="heroes" />
+        </div>
     )
 }
